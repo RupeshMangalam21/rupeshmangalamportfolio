82cba5ac5045fa5d226d7e3d6da795df11f2e53b
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-06-09 04:42:47 +0000

    added icons in skills component(needs problem-fix)

--- resume-portfolio/src/Components/SkillsSection.tsx
+++ resume-portfolio/src/Components/SkillsSection.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import '../Styles/SkillsSection.css';
+import { SiReact, SiHtml5, SiCss3, SiJavascript, SiGit, SiGithub, SiFirebase } from 'react-icons/si';
+import { AiOutlineApi } from 'react-icons/ai';
+import { FiLayers } from 'react-icons/fi';
 
 const SkillsSection: React.FC = () => {
   return (
     <div className='skill'>
       <h2>Skills</h2>
       <ul>
-        <li>React</li>
-        <li>TypeScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Git</li>
-        <li>Github</li>
-        <li>Firebase</li>
-        <li>API</li>
-        <li>Prompt Engineering</li>
+        <li><SiReact className="skill-icon" /> React</li>
+        <li><SiHtml5 className="skill-icon" /> HTML</li>
+        <li><SiCss3 className="skill-icon" /> CSS</li>
+        <li><SiJavascript className="skill-icon" /> JavaScript</li>
+        <li><SiGit className="skill-icon" /> Git</li>
+        <li><SiGithub className="skill-icon" /> Github</li>
+        <li><SiFirebase className="skill-icon" /> Firebase</li>
+        <li><AiOutlineApi className="skill-icon" /> API</li>
+        <li><FiLayers className="skill-icon" /> Prompt Engineering</li>
       </ul>
     </div>
   );
