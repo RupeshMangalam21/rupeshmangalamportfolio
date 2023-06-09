55b0134f6d8218ce393ec49210554257aaf6ea19
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-06-09 09:48:07 +0000

    added loading animation to webpage added Education section ui and icons

--- resume-portfolio/src/App.tsx
+++ resume-portfolio/src/App.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { gsap } from 'gsap';
 import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
+import './Loading.css';
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating loading time
+    setTimeout(() => {
+      setIsLoading(false); // Set isLoading to false to hide the loading animation
+    }, 2000); // Replace this with your actual loading time or API request
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      // Animation complete, do any cleanup if necessary
+      return;
+    }
+
+    // Animate the loading octagon
+    const octagon = document.getElementById('loading-octagon');
+    if (octagon) {
+      const tl = gsap.timeline({ repeat: -1 });
+      tl.to(octagon, { rotation: '+=360', duration: 2, ease: 'linear' });
+    }
+  }, [isLoading]);
+
   return (
     <div>
-      <Navbar />
-      <HomePage />
+      {isLoading ? (
+        <div className="loading-screen">
+          <svg id="loading-octagon" className="loading-octagon" viewBox="0 0 50 50">
+            <polygon points="15 5 35 5 45 15 45 35 35 45 15 45 5 35 5 15" stroke="#64ffda" strokeWidth="2" fill="transparent" />
+          </svg>
+          <div className="loading-text">RM</div>
+        </div>
+      ) : (
+        // Render your content when loading is complete
+        <div>
+          <Navbar />
+          <HomePage />
+        </div>
+      )}
     </div>
   );
 };
--- resume-portfolio/src/Components/EducationSection.tsx
+++ resume-portfolio/src/Components/EducationSection.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { FaGraduationCap } from 'react-icons/fa';
+import { FaCertificate } from 'react-icons/fa';
+import ibmLogo from '../Assets/images/icons8-ibm.svg';
+import btsLogo from '../Assets/images/icons8-bootstrap.svg';
 import '../Styles/EducationSection.css';
 
 const EducationSection: React.FC = () => {
@@ -20,7 +23,7 @@ const EducationSection: React.FC = () => {
               </div>
               <h3 className="items-title">
                 <a
-                  href="/"
+                  href="https://coursera.org/share/5cbeaf4900397fd6d2e33fde2013be34"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -36,6 +39,76 @@ const EducationSection: React.FC = () => {
                 <li>2021-2025</li>
                 <li>Software Engineering</li>
                 <li>Fullstack Development</li>
+              </ul>
+            </footer>
+          </div>
+        </li>
+        <li className="gJHaRi">
+          <div className="items-inner">
+            <header style={{ marginBottom: '-95px' }}>
+              <div className="items-top">
+                <div className="folder">
+                <img src={btsLogo} alt="Bootstrap Logo" className="icon-ibm"/>                
+                </div>
+                <div className="items-links">
+                  {/* Remove the entire <a> element */}
+                </div>
+              </div>
+              <h3 className="items-title">
+                <a
+                  href="https://coursera.org/share/5cbeaf4900397fd6d2e33fde2013be34"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Front-End Web UI Frameworks and Tools: Bootstrap 4
+                </a>
+              </h3>
+              <div className="items-description">
+                <p>A proficiency course offered by The Hong Kong University of Science and Technology, part of a full stack certification course.</p>
+              </div>
+            </header>
+            <footer>
+              <ul className="items-tech-list">
+                <li>Summer of 2022</li>
+                <li>Bootstrap</li>
+                <li>Node.Js</li>
+                <li>Jquery</li>
+                <li>SASS</li>
+              </ul>
+            </footer>
+          </div>
+        </li>
+        <li className="gJHaRi">
+          <div className="items-inner">
+            <header style={{ marginBottom: '-95px' }}>
+              <div className="items-top">
+                <div className="folder">
+                <img src={ibmLogo} alt="IBM Logo" className="icon-ibm"/>
+                </div>
+                <div className="items-links">
+                  {/* Remove the entire <a> element */}
+                </div>
+              </div>
+              <h3 className="items-title">
+                <a
+                  href="https://coursera.org/share/5cbeaf4900397fd6d2e33fde2013be34"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  IBM Full Stack Software Developer Professional Certificate
+                </a>
+              </h3>
+              <div className="items-description">
+                <p>A Professional Certification course offered by IBM through Coursera.</p>
+              </div>
+            </header>
+            <footer>
+              <ul className="items-tech-list">
+                <li>Summer of 2023</li>
+                <li>Cloud Native</li>
+                <li>Full-Stack Development</li>
+                <li>Devops</li>
+                <li>Django</li>
               </ul>
             </footer>
           </div>
