8df913f1383e6c6a1dbb224485bd03b17efbc703
Rupesh Mangalam <91172425+RupeshMangalam21@users.noreply.github.com>
2023-06-09 10:34:15 +0000

    added content on projects section added ui styling and modified margins

--- resume-portfolio/src/Components/ProjectsSection.tsx
+++ resume-portfolio/src/Components/ProjectsSection.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
 import '../Styles/ProjectsSection.css';
 
 const ProjectsSection: React.FC = () => {
+  const projects = [
+    {
+      title: 'AVAH - Healthcare Management System',
+      description: 'Ava-H: Advanced Virtual Administration for Healthcare. Web-based E-Healthcare Management System.',
+      imageUrl: 'https://i.postimg.cc/B6qvLyZR/Avah-demoimage.png',
+      demoUrl: 'https://e-healthcare-management-system-avah.vercel.app/',
+      codeUrl: 'https://github.com/RupeshMangalam21/e-healthcare-management-system',
+    },
+    {
+      title: 'Resume Portfolio Website',
+      description: 'A Personal Portfolio Website made with React and Typescript.',
+      imageUrl: 'https://i.postimg.cc/RZJrdTbh/portfoliowebsite.png',
+      demoUrl: 'https://e-healthcare-management-system-avah.vercel.app/',
+      codeUrl: 'https://github.com/RupeshMangalam21/rupeshmangalamportfolio',
+    },
+    // Add more projects
+  ];
+
   return (
-    <div>
+    <div className="projects">
       <h2>Projects</h2>
-      <ul>
-        <li>
-          <h3>Project 1</h3>
-          <p>Description: Lorem ipsum dolor sit amet.</p>
-        </li>
-        <li>
-          <h3>Project 2</h3>
-          <p>Description: consectetur adipiscing elit.</p>
-        </li>
-        {/* Add more projects */}
-      </ul>
+      <div className="project-cards">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            imageUrl={project.imageUrl}
+            demoUrl={project.demoUrl}
+            codeUrl={project.codeUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 };
